2eecc913a5d16613bc1623adb2c0d7a4edd9a517
Adarsh Navneet Sinha <adarsh91094@gmail.com>
2022-01-03 11:33:15 +0530

    LOOP Programs

---
+++ LOOPS/factorial.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num;
+    cin>>num;
+    int factorial=1;
+    for(int i=1; i<=num; i++){
+        factorial*=i;
+    }
+    cout<<factorial<<endl;
+    return 0;
+}
---
+++ LOOPS/n_batural_no_sum.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int num;
+    cin>>num;
+    int sum = 0;
+    for(int i=1; i<=num; i++)
+    sum+=num;
+    cout<<"The sum of n natural no is "<< sum <<endl;
+    return 0;
+}
