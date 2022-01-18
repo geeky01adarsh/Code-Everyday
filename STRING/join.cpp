242faaa319a87fb32ac35d5eb880ad995e660794
Adarsh Navneet Sinha <adarsh91094@gmail.com>
2022-01-18 13:29:48 +0530

    string que

---
+++ STRING/join.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+int main(){
+ll t;
+cin>>t;
+// t=1;
+while(t--){
+string s1, s2;
+cin>>s1>>s2;
+string s = s1+s2;
+cout<<s<<endl;
+}
+return 0;
+}
---
+++ STRING/string_len.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s;
+    getline(cin,s);
+    cin.ignore();
+    cout<<s.length()<<endl;
+}
